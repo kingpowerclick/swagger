6f741349adc1410b16f8b3c1744a6abf13779fcc
Unknown <npraiwan@outlook.com>
2017-06-18 18:59:49 +0700

    remove extractText plugin coz by swaggerUI style not compatible

--- webpack/prod.config.babel.js
+++ webpack/prod.config.babel.js
@@ -1,6 +1,5 @@
 import webpack from 'webpack';
 import merge from 'webpack-merge';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import CompressionPlugin from 'compression-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import StyleLintPlugin from 'stylelint-webpack-plugin';
@@ -16,26 +15,20 @@ export default merge({
   module: {
     rules: [{
       test: /\.css$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: [{
-          loader: 'css-loader',
-          query: {
-            modules: true,
-          },
-        }, {
-          loader: 'postcss-loader',
-        }],
-      }),
+      use: [{
+        loader: 'style-loader',
+      }, {
+        loader: 'css-loader',
+      }, {
+        loader: 'postcss-loader',
+      }],
     }, {
       test: /\.(jpe?g|png|gif|svg)$/i,
-      use: ExtractTextPlugin.extract({
-        use: [{
-          loader: 'file-loader',
-        }, {
-          loader: 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
-        }],
-      }),
+      use: [{
+        loader: 'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+      }, {
+        loader: 'image-webpack-loader?bypassOnDebug&optimizationLevel=7&interlaced=false',
+      }],
     }],
   },
 
@@ -49,7 +42,6 @@ export default merge({
       failOnError: true,
     }),
 
-    new ExtractTextPlugin({ filename: 'bundle-[hash].css', allChunks: true }),
     new webpack.optimize.OccurrenceOrderPlugin(true),
 
     new webpack.optimize.UglifyJsPlugin({
